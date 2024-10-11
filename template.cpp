9180db00351e494cb066c52e5f659eaf68013d75
Ádrian Gama <adrian.gama.se@gmail.com>
2024-10-11 16:31:38 -0300

    add code template

---
+++ template.cpp
@@ -0,0 +1,9 @@
+#pragma GCC optimize("O3")
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+    std::ios_base::sync_with_stdio(false);
+    std::cin.tie(NULL);
+    std::cout.tie(NULL);
+}
